15c0418a7017edc052086627d47c8a833937bee1
Andy Litzinger <andy.litzinger@gmail.com>
2011-11-27 22:33:47 -0800

    added color differencing for each state, completed the diagram calls for each state transition method

--- lib/zombie-state.rb
+++ lib/zombie-state.rb
@@ -10,7 +10,8 @@
 require 'workflow'
 class ZombieState
   include Workflow
-  def self.create_workflow_diagram(klass, cur_state = nil, target_dir='.', graph_options='rankdir="LR", size="7,11.6", ratio="fill"')
+
+  def self.create_workflow_diagram(klass, cur_state = nil, cur_fill_color = "white", target_dir='.', graph_options='rankdir="LR", size="7,11.6", ratio="fill"')
     workflow_name = "#{klass.name}_workflow".gsub('/', '_')
     fname = File.join(target_dir, "generated_#{workflow_name}")
     File.open("#{fname}.dot", 'w') do |file|
@@ -25,7 +26,7 @@ digraph #{workflow_name} {
         unless state.name.to_s == cur_state.to_s
 		  file.puts %Q{  #{state.name} [label="#{state.name}"];}
 		else
-		  file.puts %Q{  #{state.name} [style=filled, fillcolor=red label="#{state.name}"];}
+		  file.puts %Q{  #{state.name} [style=filled, fillcolor=#{cur_fill_color} label="#{state.name}"];}
 		end
         state.events.each do |event_name, event|
           meta_info = event.meta
@@ -40,14 +41,9 @@ digraph #{workflow_name} {
       file.puts "}"
       file.puts
     end
-    `dot -Tpdf -o'#{fname}.pdf' '#{fname}.dot'`
-    puts "
-Please run the following to open the generated file:
-
-open '#{fname}.pdf'
-
-"
-  end
+    # this call seems to fuddle windows- possibly due to path slash direction
+	#`dot -Tpdf -o'#{fname}.pdf' '#{fname}.dot'`
+ end
 
   workflow do
     state :human do
@@ -109,68 +105,68 @@ open '#{fname}.pdf'
     puts 'human/not infected'
     @z_scale = 0
     # update_graphviz_data
-	#self.class.create_workflow_diagram(ZombieState, "human")
+    self.class.create_workflow_diagram(ZombieState, "human", "green")
   end
 
   def exposed
     puts 'exposed to zombie'
     @z_scale = 1
     # update_graphviz_data
-	self.class.create_workflow_diagram(ZombieState, "exposed")
+	self.class.create_workflow_diagram(ZombieState, "exposed", "yellow")
   end
 
   def exposure_level_1
     puts 'low level of risk for infection'
     @z_scale = 3
     # update_graphviz_data
-	self.class.create_workflow_diagram(ZombieState, "risk_level_1")
-  end
-  
-  def exposure_level_1
-      puts 'low level of risk for infection'
-    z_scale = 3
-    # update_graphviz_data
-	self.class.create_workflow_diagram(ZombieState, "risk_level_1")
+    self.class.create_workflow_diagram(ZombieState, "risk_level_1", "yellow")
   end
   
   def exposure_level_2
     puts 'moderate level of risk for infection'
     @z_scale = 5
     # update_graphviz_data
+    self.class.create_workflow_diagram(ZombieState, "risk_level_2", "orange")
   end
   
   def exposure_level_3
     puts 'high level of risk for infection'
     @z_scale = 7
     # update_graphviz_data
+    self.class.create_workflow_diagram(ZombieState, "risk_level_3", "magenta")
   end
   
   def quarantine
     puts 'in quarantine for 1 week'
     # z_scale is not changed until leaving quarantine
     # update_graphviz_data
+    self.class.create_workflow_diagram(ZombieState, "quarantine", "magenta")
   end
   
   def infected
     puts 'infected with zombie virus'
     @z_scale = 8
     # update_graphviz_data
+    self.class.create_workflow_diagram(ZombieState, "proto_zombie", "red")
   end
   
   def proto_zombie
     puts 'starting to manifest zombie traits'
     @z_scale = 9
     # update_graphviz_data
+    self.class.create_workflow_diagram(ZombieState, "proto_zombie", "red")
   end
   
   def zombie
     puts 'full zombie phenotype'
     @z_scale = 10
     # update_graphviz_data
+    self.class.create_workflow_diagram(ZombieState, "zombie", "red")
   end
   
   def initialize
     @z_scale = 0
+    self.class.create_workflow_diagram(ZombieState, "human", "green")
       
   end
   
--- spec/diagram_spec01.rb
+++ spec/diagram_spec01.rb
@@ -2,4 +2,24 @@ require "rubygems"
 require "bundler/setup"
 require_relative '../lib/zombie-state.rb'
 
-ZombieState.create_workflow_diagram(ZombieState)
+describe "it should color the current state " do
+  subject { ZombieState.new }
+
+  it "should color the initial state green" do
+
+    subject
+  
+  end
+
+  it "should color the exposed state yellow" do
+
+    subject.exposed!
+    subject.exposure_level_1!
+    subject.quarantine!
+    subject.proto_zombie!
+    subject.zombie!
+  
+  end
+
+
+end
